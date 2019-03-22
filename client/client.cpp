3c3000626a0cbfed99c906898eee8fe185745aaf
Rutvik Patel <43623225+patel-rutvik@users.noreply.github.com>
2019-03-21 23:32:46 -0600

    Add files via upload

---
+++ client/client.cpp
@@ -0,0 +1,66 @@
+#include "util.h"
+#include "lcd_image.h"
+
+void setup() {
+  // initialize Arduino
+  init();
+
+  // initialize zoom pins
+  pinMode(clientpins::left_pin, INPUT_PULLUP);
+  pinMode(clientpins::right_pin, INPUT_PULLUP);
+
+  // initialize joystick pins and calibrate centre reading
+  pinMode(clientpins::joy_button_pin, INPUT_PULLUP);
+  // x and y are reverse because of how our joystick is oriented
+  
+
+  // initialize serial port
+  Serial.begin(9600);
+  Serial.flush();  // get rid of any leftover bits
+
+
+  tft.begin();
+  tft.setRotation(3);
+  tft.fillScreen(ILI9341_WHITE);
+
+  // initialize SD card
+  if (!SD.begin(clientpins::sd_cs)) {
+      Serial.println("Initialization has failed. Things to check:");
+      Serial.println("* Is a card inserted properly?");
+      Serial.println("* Is your wiring correct?");
+      Serial.println("* Is the chipSelect pin the one for your shield or module?");
+      while (1) {}  // nothing to do here, fix the card issue and retry
+  }
+}
+
+void textInit() {
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+The textInit function takes in no paramaters.
+
+It does not return any parameters.
+
+This function is responsible for taking care of all the initial duties of
+correctly setting up the text and formatting it accordingly. The test used in
+this exercise is all in one colour and size.
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+    tft.setTextColor(ILI9341_BLACK);
+    tft.setTextSize(1);
+    tft.setFont(&FreeSansBold9pt7b);
+}
+
+void welcomeScreen() {
+    tft.setCursor(displayconsts::tft_width/3, displayconsts::tft_height/4);
+    textInit();
+    tft.print("Welcome!");
+    tft.setCursor(displayconsts::tft_width/6, displayconsts::tft_height/4 + 50);
+    tft.print("Periodic Table Database");
+    tft.setCursor(displayconsts::tft_width/8, displayconsts::tft_height/2 + 40);
+    //tft.setFont(2);
+    tft.print("By: Rutvik Patel and Kaden Dreger");
+}
+
+int main() {
+    setup();
+    welcomeScreen();
+    return 0;
+}
---
+++ client/util.h
@@ -0,0 +1,57 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <Arduino.h>
+#include <Adafruit_ILI9341.h>
+#include <SD.h>
+#include <Fonts/FreeSansBold9pt7b.h>
+
+namespace clientpins {
+    // Pins and for the zoom in and out buttons.
+    const uint8_t left_pin = 2;
+    const uint8_t right_pin = 3;
+
+    // the pins used to connect to the AdaFruit display
+    const uint8_t sd_cs = 6;
+    const uint8_t tft_cs = 10;
+    const uint8_t tft_dc = 9;
+
+    // analog pins for joystick axes
+    const uint8_t joy_x_pin = A1;
+    const uint8_t joy_y_pin = A0;
+
+    // joystick button pin
+    const uint8_t joy_button_pin = 8;
+};
+
+// constants related to the display
+namespace displayconsts {
+    // physical dimensions of the tft display
+    const int16_t tft_width = 320;
+    const int16_t tft_height = 240;
+
+    // dimensions of the bottom "display" that has the FROM? or TO? prompt
+    const int16_t msg_width = tft_width;
+    const int16_t msg_height = 24;
+
+    // the display window size for the map of edmonton
+    const int16_t display_width = tft_width;
+    const int16_t display_height = tft_height - msg_height;
+
+    // amount of "padding" around the display, the cursor should
+    // not move into the padding (will cause a map view nudge
+    // unless we are near the boundary of the map itself)
+    const int16_t padding = 2;
+
+
+    // the minimum and maximum display coordinates we allow the
+    // cursor to occupy, anything outside this should nudge the screen
+    const int16_t min_x = padding;
+    const int16_t max_x = display_width - padding;
+    const int16_t min_y = padding;
+    const int16_t max_y = display_height - padding;
+};
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(clientpins::tft_cs, clientpins::tft_dc);
+
+#endif //UTIL_H
