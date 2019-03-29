1280b6e1bcc2221813f58b3fb7194278dd7aa0f2
Rutvik Patel <43623225+patel-rutvik@users.noreply.github.com>
2019-03-28 21:36:52 -0600

    Changed colours

--- client/client.cpp
+++ client/client.cpp
@@ -181,23 +181,23 @@ void blankCard() {
     // blank white card
     //tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, ILI9341_WHITE);
     if (element.type == "Nonmetal") {  // Oxygen
-        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(0, 118, 0));
+        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(128, 235, 128));
     } else if (element.type == "Transition Metal") {  // Gold
-        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(128, 0, 0));
+        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(255, 120, 120));
     } else if(element.type == "Noble Gas") {  // Helium
-        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(0, 0, 235));
+        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(173, 216, 250));
     } else if (element.type == "Alkali Metal") {  // Lithium
-        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(225, 135, 0));
+        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(255, 180, 98));
     } else if(element.type == "Alkaline Earth Metal") {  // Calcium
-        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(0, 90, 90));
+        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(0, 140, 140));
     } else if (element.type == "Halogen") {  // Fluorine
-        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(150, 0, 0));
+        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(50, 255, 241));
     } else if (element.type == "Lanthanide") {
         tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(255, 255, 255));
     } else if(element.type == "Metal") {  // Tin
-        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(101, 67, 33));
+        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(250, 250, 100));
     } else if (element.type == "Metalloid") { // Boron
-        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(108, 0, 108));
+        tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(197, 139, 231));
     } else if (element.type == "Actinide") {
         tft.fillRect(0, 0, displayconsts::tft_width - sidebar, displayconsts::tft_height, tft.color565(255, 255, 255));
     } else if (element.type == "Transactinide") {
